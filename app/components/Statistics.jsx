4231febb12ed81ebd201100f12aa7075a96eebfd
stephen james <jeratdevworks@gmail.com>
2025-03-27 17:17:01 +0100

    third commit

--- app/components/Statistics.jsx
+++ app/components/Statistics.jsx
@@ -3,8 +3,10 @@ import { Trophy, Clipboard, CheckCircle } from "lucide-react";
 export default function Statistics({ rank, percentile, score }) {
   return (
     <div className="bg-white p-4 md:p-6 rounded-lg shadow-md w-full mx-auto">
-      <h2 className="text-lg font-bold mb-4">Quick Statistics</h2>
-      <div className="grid grid-cols-3 ml-14">
+      <h2 className="text-lg font-bold mb-4 text-center md:text-left">
+        Quick Statistics
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div className="flex items-center space-x-3">
           <Trophy className="w-8 h-8 text-yellow-500" />
           <div className="flex flex-col">
